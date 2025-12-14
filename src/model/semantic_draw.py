f2ac49f80276aa31df0d4af54d5afcc0cf53b301
marcomorandin <emmeemmenove@gmail.com>
2025-12-14 16:14:21 +0100

    .

--- src/model/semantic_draw.py
+++ src/model/semantic_draw.py
@@ -150,6 +150,7 @@ class SemanticDraw(nn.Module):
             self.i2t_model = None
 
         self.pipe = load_model(model_key, self.sd_version, self.device, self.dtype)
+        self.pipe.scheduler = get_scheduler(self.pipe)
 
         self.pipe.load_lora_weights(lora_key, weight_name=lora_weight_name, adapter_name='lcm')
         self.pipe.fuse_lora(
